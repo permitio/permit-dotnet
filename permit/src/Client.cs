0cab45e9e24b991f7551ec1819cc50ae2a42b734
Oded BD <oded@permit.io>
2022-01-23 12:47:53 +0200

    make client attrs public with private set

--- permit/src/Client.cs
+++ permit/src/Client.cs
@@ -5,10 +5,10 @@ namespace Permit
     {
         public const string DEFAULT_PDP_URL = "http://localhost:7000";
 
-        Config Config { get; }
-        public Enforcer Enforcer;
-        public Cache Cache;
-        public API Api;
+        public Config Config { get; private set; }
+        public Enforcer Enforcer { get; private set; }
+        public Cache Cache { get; private set; }
+        public API Api { get; private set; }
 
         public Client(string token, string pdp = DEFAULT_PDP_URL, bool debugMode = false, string level = "info", string label = "permitio-sdk", string defaultTenant = "default", bool logAsJson = false)
         {
