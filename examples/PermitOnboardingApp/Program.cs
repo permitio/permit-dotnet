2a27e9cef31f08c56b6c314dd6a39d0f1250adfd
Oded BD <oded@permit.io>
2022-01-27 13:09:19 +0200

    update license, update demo app and move to folder

---
+++ examples/PermitOnboardingApp/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PermitOnboardingApp
+{
+    class HttpServer
+    {
+        public static HttpListener listener;
+        public static string url = "http://localhost:4000/";
+        public static string pageData = "User {0} is {1} to {2} {3}";
+
+        public static async Task HandleIncomingConnections()
+        {
+            bool runServer = true;
+
+            while (runServer)
+            {
+                // Will wait here until we hear from a connection
+                HttpListenerContext ctx = await listener.GetContextAsync();
+                HttpListenerResponse resp = ctx.Response;
+
+                string testToken = "";
+                string testUserId = "tester@test.com";
+                PermitSDK.Permit permit = new PermitSDK.Permit(
+                    testToken,
+                    "http://localhost:7000",
+                    "default",
+                    true
+                );
+                bool permitted = await permit.Check(
+                    testUserId,
+                    "{{diff_action}}",
+                    "{{diff_resource}}"
+                );
+                if (permitted)
+                {
+                    await sendResponseAsync(
+                        resp,
+                        200,
+                        String.Format(
+                            pageData,
+                            testUserId,
+                            "Permitted",
+                            "{{diff_action}}",
+                            "{{diff_resource}}"
+                        )
+                    );
+                }
+                else
+                {
+                    await sendResponseAsync(
+                        resp,
+                        403,
+                        String.Format(
+                            pageData,
+                            testUserId,
+                            "NOT Permitted",
+                            "{{diff_action}}",
+                            "{{diff_resource}}"
+                        )
+                    );
+                }
+            }
+        }
+
+        public static async Task sendResponseAsync(
+            HttpListenerResponse resp,
+            int returnCode,
+            string responseContent
+        )
+        {
+            byte[] data = Encoding.UTF8.GetBytes(responseContent);
+            resp.StatusCode = returnCode;
+            await resp.OutputStream.WriteAsync(data, 0, data.Length);
+            resp.Close();
+        }
+
+        public static void Main(string[] args)
+        {
+            // Create a Http server and start listening for incoming connections
+            listener = new HttpListener();
+            listener.Prefixes.Add(url);
+            listener.Start();
+            Console.WriteLine("Listening for connections on {0}", url);
+
+            // Handle requests
+            Task listenTask = HandleIncomingConnections();
+            listenTask.GetAwaiter().GetResult();
+
+            // Close the listener
+            listener.Close();
+        }
+    }
+}
--- src/permit/Models/PermitCheck.cs
+++ src/permit/Models/PermitCheck.cs
@@ -1,8 +1,31 @@
 ﻿namespace PermitSDK.Models
 {
+    public class PermitCheckQuery
+    {
+        public string action { get; private set; }
+        public ResourceInput resource { get; private set; }
+        public User user { get; private set; }
+
+        public PermitCheckQuery(string action, ResourceInput resource, User user)
+        {
+            this.action = action;
+            this.resource = resource;
+            this.user = user;
+        }
+
+        public PermitCheckQuery() { }
+    }
+
     public class PermitCheck
     {
         public bool allow { get; private set; }
+        public PermitCheckQuery query { get; private set; }
+
+        public PermitCheck(bool allow, PermitCheckQuery query)
+        {
+            this.allow = allow;
+            this.query = query;
+        }
 
         public PermitCheck() { }
     }
